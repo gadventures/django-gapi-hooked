523e86d9f074bbf7eeb885ee124102a4ec3e0a4a
Bartek Ciszkowski <bart.ciszk@gmail.com>
2016-03-14 15:41:11 -0400

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-hooked',
-    version='0.1.0',
+    version='0.2.0',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-gapi-hooked',
