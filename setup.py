88240f3d990e25dffc3cfbbf969c6b797e145f62
ieatkittens <samuelm@gadventures.com>
2016-03-17 14:51:49 -0400

    Moved version to variable to avoid repetition

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
+current_version = '0.2.0'
+
 setup(
     name='django-gapi-hooked',
-    version='0.2.0',
+    version=current_version,
     author='G Adventures',
     author_email='software@gadventures.com',
     description='A tiny library to add a G Adventures API Web Hook receiver view to your code base.',
-    download_url='https://github.com/gadventures/django-gapi-hooked/tarball/0.2.0',
+    download_url='https://github.com/gadventures/django-gapi-hooked/tarball/%s' % current_version,
     url='https://github.com/gadventures/django-gapi-hooked',
     packages=[
         'django-gapi-hooked',
