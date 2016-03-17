2c5e284a94297aee2df2c786f6915ad6620c1a8b
ieatkittens <samuelm@gadventures.com>
2016-03-17 13:38:03 -0400

    Initial commit of changes needed to publish to pypi
    Added setup.cfg to point pypi to the readme
    
    Added Download link, description, updated package name
    
    Need to save if you want all your changes included

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from setuptools import setup
 
-setup(name='django-hooked',
+setup(
+    name='django-gapi-hooked',
     version='0.2.0',
     author='G Adventures',
     author_email='software@gadventures.com',
+    description='A tiny library to add a G Adventures API Web Hook receiver view to your code base.',
+    download_url='https://github.com/gadventures/django-gapi-hooked/tarball/0.2.0',
     url='https://github.com/gadventures/django-gapi-hooked',
     packages=[
-        'hooked',
+        'django-gapi-hooked',
     ],
     test_suite='hooked.runtests.main',
     install_requires=[
         'Django>=1.6',
+    ],
+    keywords=[
+        'gapi',
+        'g adventures',
+        'g api',
+        'gapipy'
     ]
 )
