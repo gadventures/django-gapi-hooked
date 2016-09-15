82989c66a1b8e8a9051923e7942515fab15d3fe5
Adam McKerlie <adammckerlie@gmail.com>
2016-09-15 11:13:16 -0400

    Raise Exceptions instead of returning them. Returning the exceptions causes an exception lower down since the Exception is returned to the events variable and used elsewhere. Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-hooked',
-    version='0.2.0',
+    version='0.3.0',
     author='G Adventures',
     author_email='software@gadventures.com',
     url='https://github.com/gadventures/django-gapi-hooked',
